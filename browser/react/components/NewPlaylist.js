a9c2c438801b62fee70688043d8e2cf2ef9320fc
Derrick Beining <derrick.beining@gmail.com>
2017-08-24 10:32:49 -0400

    finished new playlist state, now on 3.Controlled Components

--- browser/react/components/NewPlaylist.js
+++ browser/react/components/NewPlaylist.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default function NewPlaylist() {
+export default class NewPlaylist extends Component {
+  constructor() {
+    super()
+    this.state = {
+      value: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  return (
-    <div className="well">
-      <form className="form-horizontal">
-        <fieldset>
-          <legend>New Playlist</legend>
-          <div className="form-group">
-            <label className="col-xs-2 control-label">Name</label>
-            <div className="col-xs-10">
-              <input className="form-control" type="text" />
+  handleChange (event) {
+    const value = event.target.value;
+    this.setState({value})
+  }
+
+  handleSubmit (event) {
+    event.preventDefault();
+    console.log(this.state.value);
+    this.setState({value: ''});
+  }
+
+  render () {
+    return (
+      <div className="well">
+        <form className="form-horizontal" onSubmit={this.handleSubmit}>
+          <fieldset>
+            <legend>New Playlist</legend>
+            <div className="form-group">
+              <label className="col-xs-2 control-label">Name</label>
+              <div className="col-xs-10">
+                <input className="form-control" type="text" value={this.state.value} onChange={this.handleChange} />
+              </div>
             </div>
-          </div>
-          <div className="form-group">
-            <div className="col-xs-10 col-xs-offset-2">
-              <button type="submit" className="btn btn-success">Create Playlist</button>
+            <div className="form-group">
+              <div className="col-xs-10 col-xs-offset-2">
+                <button type="submit" className="btn btn-success">Create Playlist</button>
+              </div>
             </div>
-          </div>
-        </fieldset>
-      </form>
-    </div>
-  );
-
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
 }
